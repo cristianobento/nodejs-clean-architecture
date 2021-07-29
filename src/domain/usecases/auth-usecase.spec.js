5ee13bf71528004e2ba6b03ec27d2d37bebdc036
Bento <cristiano@cristianooliveira.com>
2021-07-29 00:21:04 -0300

    ensure AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -9,20 +9,19 @@ class AuthUseCase {
     if (!email) {
       throw new MissingParamError('email')
     }
-
     if (!password) {
       throw new MissingParamError('password')
     }
-
     if (!this.loadUserByEmailRepository) {
       throw new MissingParamError('loadUserByEmailRepository')
     }
-
     if (!this.loadUserByEmailRepository.load) {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
-
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -68,5 +67,11 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
     expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@mail.com', 'any_password')
+    expect(accessToken).toBeNull()
   })
 })
